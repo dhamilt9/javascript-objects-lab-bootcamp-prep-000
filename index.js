f422894774b4d662d612b6fe0f2c0278b2899f8c
Daniel Hamilton <danhamilton16@gmail.com>
2018-10-08 20:56:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function updateObjectWithKeyAndValue(object, key, value){
   updatedObj[key]=value;
   return updatedObj;
 }
+
 destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object=Object.assign({}, object, [key], value)
 }
